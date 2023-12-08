a4863793e07970ca422c371bdda6b1ffd2f9ea11
Bacha-K <148437277+Bacha-K@users.noreply.github.com>
2023-12-08 12:07:36 +0400

    Add files via upload

---
+++ 0 Level - Data types.js
@@ -0,0 +1,157 @@
+const value = '30 Days Of JavaScript'
+
+//3***
+
+//console.log(value.length);
+
+//4-5***
+
+//1.
+//console.log(value.toUpperCase());
+//console.log(value.toLowerCase());
+
+//2.
+//let lowerCaseValue = value.toLowerCase()
+//let UpperCaseValue = value.toUpperCase()
+//console.log(value)
+//console.log(lowerCaseValue)
+//console.log(UpperCaseValue)
+
+//6***
+//1.
+//console.log(value.substring (0,2));
+
+//console.log(value.substr (0,3));
+
+//2.
+//let log = console.log
+//log (value.substring(0,3));
+//log (value.substr (0,3));
+
+//7***
+
+//1.
+//console.log(value.substring (3,21));
+
+//8***
+
+//1.
+//const result = value.includes("Script");
+//console.log(result);
+
+//9-10***
+
+//1.
+//const words = value.split (' ');
+//console.log(words[2]);
+//2.
+//const words = value.split(' ');
+//console.log(words);
+
+//11***
+
+//const string = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+
+//const comma = string.split(' ');
+//console.log(comma);
+
+//12***
+
+//1.
+//const NewValue = value.replace("JavaScript", "Phyton");
+//console.log(NewValue);
+
+
+//13***
+
+//1.
+//value.charAt(15)
+
+//2.
+//let character = value.charAt(15);
+//console.log(character);
+
+//3.
+//value[15];
+
+//14***
+
+//1.
+//let unicodeValue = value.charCodeAt(11);
+//console.log(unicodeValue);
+
+//2.
+//console.log(value.charCodeAt(11));
+
+//15***
+
+//1.
+//let spot = value.indexOf("a");
+//console.log(spot);
+
+//16***
+
+//1.
+//let spot = value.lastIndexOf("a");
+//console.log(spot);
+
+//17***
+
+//1.
+//const word = 'You cannot end a sentence with because because because is a conjunction'
+//let spot = word.indexOf("because")
+//console.log(spot);
+
+//18***
+
+//1.
+//const word = 'You cannot end a sentence with because because because is a conjunction'
+//let spot = word.lastIndexOf("because");
+//console.log(spot);
+
+//19***
+
+//1.
+//let word = 'You cannot end a sentence with because because because is a conjunction'
+//let position = word.search("because");
+//console.log(position);
+
+//20***
+
+//1.
+//const word = ' 30 Days Of JavaScript '
+//let str = word.trim();
+//console.log(str);
+
+//21***
+
+//1.
+//const result = value.startsWith("30", 0);
+//console.log(result);
+
+//22***
+
+//1.
+//const result = value.endsWith("JavaScript");
+//console.log(result);
+
+//23***
+
+//1.
+//const result = value.match(/a/g);
+//console.log(result);
+
+//24***
+
+//1.
+//const value = "30 Days of"
+//const value1 = (" ", "JavaScript")
+//const result = value.concat(" ", value1);
+//console.log(result);
+
+//25***
+
+//1.
+//const result = value.repeat(2);
+//console.log(result); 
+
